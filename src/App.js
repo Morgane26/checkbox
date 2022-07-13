76283f89ceb00bed8b09ea777f15334c396c986f
Morgane Perenzoni <37402070+Morgane26@users.noreply.github.com>
2022-07-13 18:28:56 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import CheckboxList from "./components/CheckboxList";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello Fulll team</h1>
+      <CheckboxList />
+    </div>
+  );
+}
---
+++ src/components/items.js
@@ -0,0 +1,18 @@
+export const Items = [
+  {
+    id: "01",
+    name: "Checkbox 1"
+  },
+  {
+    id: "02",
+    name: "Checkbox 2"
+  },
+  {
+    id: "03",
+    name: "Checkbox 3"
+  },
+  {
+    id: "04",
+    name: "Checkbox 4"
+  }
+];
